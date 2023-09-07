2d3ed2dec25ff76d068cae804a9727c0b67c7580
Baspetros <barnifull@gmail.com>
2023-09-07 14:25:14 -0700

    advan task.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 /**
  * main - this is the main function
  *
- * Return: 1 failure
+ * Return: 1 failures
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 59)
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 	return (1);
 }
