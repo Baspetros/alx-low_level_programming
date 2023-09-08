f8b86adab48434ce362cafea94c6fc955d9bf47d
Baspetros <barnifull@gmail.com>
2023-09-08 08:20:21 -0700

    last advan task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 as usual
+ */
+int main(void)
+{
+	int firstdigit = 0, seconddigit;
+
+	while (firstdigit <= 99)
+	{
+		seconddigit = firstdigit;
+		while (seconddigit <= 99)
+		{
+			if (seconddigit != firstdigit)
+			{
+				putchar((firstdigit / 10) + 48);
+				putchar((firstdigit % 10) + 48);
+				putchar(' ');
+				putchar((seconddigit / 10) + 48);
+				putchar((seconddigit % 10) + 48);
+
+				if (firstdigit != 98 || seconddigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			seconddigit++;
+		}
+		firstdigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
