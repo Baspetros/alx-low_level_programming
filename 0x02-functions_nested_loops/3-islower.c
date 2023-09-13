576a2a464a17634477777133ac1c32f0dcdbf716
Baspetros <barnifull@gmail.com>
2023-09-13 07:29:00 -0700

    3 task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower(int c) - main function
+ * @c: parameter of the function
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
