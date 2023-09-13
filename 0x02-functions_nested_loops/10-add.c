756c806b539ae856f7b307009e3e49650f4e85d0
Baspetros <barnifull@gmail.com>
2023-09-13 10:28:38 -0700

    10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - main function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
