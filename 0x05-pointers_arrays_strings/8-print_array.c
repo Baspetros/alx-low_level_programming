20b5b6ebd977206b8995a7216237bf22cf3f13e5
Baspetros <barnifull@gmail.com>
2023-09-18 15:00:37 -0700

    8 task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,13 +9,11 @@ void print_array(int *a, int n)
 {
 	int z;
 
-	for (z = 0; z < (n - 1); z++)
+	for (z = 0 ; z < n ; z++)
 	{
-		printf("%d,", a[z]);
-	}
-	if (z == (n - 1))
-	{
-		printf("%d", a[n - 1]);
+		printf("%d", a[z]);
+		if (z < n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
