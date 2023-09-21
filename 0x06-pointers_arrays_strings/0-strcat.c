ca3f9d14f56af60339422603254e7d2688e69c9c
Baspetros <barnifull@gmail.com>
2023-09-21 05:59:42 -0700

    0 task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - main function
+ * @dest: parameter 1
+ * @src: parameter 2
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
